0c92fb08b503562960bd09c3d67d185a154cb67b
Samdan <pri.samdan@gmail.com>
2018-10-18 01:14:31 +0800

    complate binary tree in array

---
+++ lab7/main.cpp
@@ -0,0 +1,54 @@
+#include "./complete_binary_tree.h"
+
+#define SIZE 7
+
+int main()
+{
+	int cbtree[SIZE] = {7, 8, 1, 4, 3, 9, 2};
+
+	cout << endl
+		 << "ROOT IS: " << endl;
+	root(cbtree, SIZE - 1);
+
+	cout << endl
+		 << endl
+		 << "LEAF IS: " << endl;
+	leaf(cbtree, 0, SIZE - 1);
+
+	cout << endl
+		 << endl
+		 << "LEVEL IS: " << endl
+		 << level(cbtree, SIZE);
+
+	cout << endl
+		 << endl
+		 << "HEIGHT IS: " << endl
+		 << height(cbtree, SIZE);
+
+	cout << endl
+		 << endl
+		 << "ANCESTOR IS: " << endl;
+	ancestor(cbtree, 6, SIZE - 1);
+
+	cout << endl
+		 << endl
+		 << "DESCENDANT IS: " << endl;
+	descendant(cbtree, 0, SIZE - 1);
+
+	cout << endl
+		 << endl
+		 << "SIBLINGS IS: " << endl;
+	siblings(cbtree, 0, SIZE - 1);
+
+	cout << endl
+		 << endl
+		 << "PARENT IS: " << endl;
+	parent(cbtree, 4, SIZE - 1);
+
+	cout << endl
+		 << endl
+		 << "CHILDREN IS: " << endl;
+	children(cbtree, 0, SIZE - 1);
+
+	return 0;
+}
