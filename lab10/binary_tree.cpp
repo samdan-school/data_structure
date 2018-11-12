60436f1418f560997de015783f8c00b8072f9f00
Samdan D <pri.samdan@gmail.com>
2018-11-13 03:32:43 +0800

    finish bst with avl insert

--- lab10/binary_tree.cpp
+++ lab10/binary_tree.cpp
@@ -30,12 +30,43 @@ class Binary_tree {
    public:
     node *root;
 
-    Binary_tree(int value) {
-        root = new node(value);
+    Binary_tree(int value) { root = new node(value); }
+
+    void insert(int value) { this->root = insert(this->root, value); }
+
+    node *left_left_swap(node *current) {
+        node *left_child = current->left_child;
+        current->left_child = left_child->right_child;
+        left_child->right_child = current;
+
+        return left_child;
     }
 
-    void insert(int value) {
-        this->root = insert(this->root, value);
+    node *left_right_swap(node *current) {
+        node *left_child_s_right_child = current->left_child->right_child;
+        current->left_child->right_child = left_child_s_right_child->left_child;
+        left_child_s_right_child->left_child = current->left_child;
+        current->left_child = left_child_s_right_child;
+
+        return left_child_s_right_child;
+    }
+
+    node *right_right_swap(node *current) {
+        node *right_child = current->right_child;
+        current->right_child = right_child->left_child;
+        right_child->left_child = current;
+
+        return right_child;
+    }
+
+    node *right_left_swap(node *current) {
+        node *right_child_s_left_child = current->right_child->left_child;
+        current->right_child->left_child =
+            right_child_s_left_child->right_child;
+        right_child_s_left_child->right_child = current->right_child;
+        current->right_child = right_child_s_left_child;
+
+        return right_child_s_left_child;
     }
 
     node *insert(node *current, int value) {
@@ -44,23 +75,33 @@ class Binary_tree {
         } else if (current->value > value) {
             current->left_child = insert(current->left_child, value);
 
-            if (height(current->left_child) - height(current->right_child) == 2) {
+            if (height(current->left_child) - height(current->right_child) ==
+                2) {
                 cout << "CUR VALUE: " << current->value << endl;
-                if (height(current->left_child->left_child) > height(current->left_child->right_child)) {
+                if (height(current->left_child->left_child) >
+                    height(current->left_child->right_child)) {
                     cout << "L L OVER" << endl;
+                    current = left_left_swap(current);
                 } else {
                     cout << "L R OVER" << endl;
+                    left_right_swap(current);
+                    current = left_left_swap(current);
                 }
             }
         } else if (current->value < value) {
             current->right_child = insert(current->right_child, value);
 
-            if (height(current->right_child) - height(current->left_child) == 2) {
+            if (height(current->right_child) - height(current->left_child) ==
+                2) {
                 cout << "CUR VALUE: " << current->value << endl;
-                if (height(current->right_child->left_child) > height(current->right_child->right_child)) {
-                    cout << "R L OVER" << endl;
-                } else {
+                if (height(current->right_child->right_child) >
+                    height(current->right_child->left_child)) {
                     cout << "R R OVER" << endl;
+                    current = right_right_swap(current);
+                } else {
+                    cout << "R L OVER" << endl;
+                    right_left_swap(current);
+                    current = right_right_swap(current);
                 }
             }
         }
@@ -70,8 +111,7 @@ class Binary_tree {
     }
 
     int height(node *start) {
-        if (start == nullptr)
-            return 0;
+        if (start == nullptr) return 0;
 
         int l = 0, r = 0;
 
@@ -85,23 +125,37 @@ class Binary_tree {
         return 1 + max(l, r);
     }
 
-    node *search_node(int value) {
-        node *current = root;
+    node *search_node(node *start, int value) {
+        node *current = start;
+
+        if (current == nullptr) {
+            return nullptr;
+        } else if (current->value == value) {
+            return current;
+        } else if (current->value > value) {
+            search_node(current->left_child, value);
+        } else if (current->value < value) {
+            search_node(current->right_child, value);
+        }
+    }
+
+    bool search_node(int value) {
+        if (search_node(this->root, value) != nullptr)
+            return true;
+        else
+            return false;
     }
 
     void print_tree(node *start, int depth = 0) {
-        cout << "DEPTH: " << depth << " - VALUE: " << start->value << " - HEIGHT: " << start->height << endl;
-        if (start->left_child)
-            print_tree(start->left_child, depth + 1);
-        if (start->right_child)
-            print_tree(start->right_child, depth + 1);
+        cout << "DEPTH: " << depth << " - VALUE: " << start->value
+             << " - HEIGHT: " << height(start) << endl;
+        if (start->left_child) print_tree(start->left_child, depth + 1);
+        if (start->right_child) print_tree(start->right_child, depth + 1);
     }
 
     void delete_node(node *start) {
-        if (start->left_child)
-            delete_node(start->left_child);
-        if (start->right_child)
-            delete_node(start->right_child);
+        if (start->left_child) delete_node(start->left_child);
+        if (start->right_child) delete_node(start->right_child);
         // cout << "DEST: " << start->value << endl;
         delete start;
     }
--- lab10/main.cpp
+++ lab10/main.cpp
@@ -6,15 +6,22 @@ using namespace std;
 int main() {
     Binary_tree bs = Binary_tree(10);
 
-    bs.insert(5);
-    bs.insert(13);
-    bs.insert(11);
+    bs.insert(9);
     bs.insert(15);
-    bs.insert(12);
+    bs.insert(13);
+    bs.insert(17);
+    bs.insert(14);
+    // bs.insert(12);
     // bs.rec_insert(bs.root, 12);
     // bs.rec_insert(bs.root, 20);
 
     bs.print_tree(bs.root);
+
+    if (bs.search_node(-1)) {
+        cout << "VALUE DOES EXISTS!" << endl;
+    } else {
+        cout << "VALUE DOES NOT EXISTS!" << endl;
+    }
 
     return 0;
 }
